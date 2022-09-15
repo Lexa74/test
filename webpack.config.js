640b585ed9b13e02422f8684333614f769712dfa
Lexa74 <you@example.com>
2022-09-15 19:47:20 +0500

    Закончил верстку без адаптива

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
 module.exports = {
-    entry: ['./script.js', './style.styl'],
+    entry: ['./script.js', './style.styl', './index.html'],
     output: {
         filename: 'bundle.js',
-        path: require('path').resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist/'),
+        assetModuleFilename: 'assets/images/[name][ext]'
     },
     module: {
         rules: [
             {
-                test: /\.styl$/,
-                loader: "stylus-loader", // compiles Styl to CSS
+                test: /.styl$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader" },
+                    { loader: "stylus-loader" }
+                ],
+                dependency: { not: ['url'] }
             },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 }       
